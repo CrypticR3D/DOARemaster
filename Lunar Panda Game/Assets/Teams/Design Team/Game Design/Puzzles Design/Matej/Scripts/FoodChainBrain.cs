917b2c6116025f1fd6e5bb428c1a62d4b388db13
gajdm <99370045+gajdm@users.noreply.github.com>
2022-07-11 20:59:28 +0200

    Random Numbers Script
    Added randomizing to the foodchain puzzle

---
+++ Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/FoodChainBrain.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodChainBrain : MonoBehaviour
+{
+    public RandomNumber tiger;
+    public RandomNumber wolf;
+    public RandomNumber fox;
+    public RandomNumber rab;
+
+    int firstNumber;
+    int secondNumber;
+    int thirdNumber;
+    int fourthNumber;
+
+    public string theCode;
+
+    public void Start()
+    {
+        firstNumber = tiger.GetNumber();
+        secondNumber = wolf.GetNumber();
+        thirdNumber = fox.GetNumber();
+        fourthNumber = rab.GetNumber();
+        theCode =firstNumber+ "" + secondNumber + "" + thirdNumber + "" + fourthNumber;
+    }
+
+}
--- Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/RandomNumber.cs
+++ Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/RandomNumber.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RandomNumber : MonoBehaviour
 {
     //Basic Logic
     int minNumber = 0;
     int maxNumber = 9;
-    public int numberOne;
-    public int numberTwo;
-    public int numberThree;
-    public int numberFour;
+    int numberOne;
+    int numberTwo;
+    int numberThree;
+    int numberFour;
 
 
     //Differentiating the discs themselves. Each Disc registers different animal as the main number.
-
+    //We need to keep track of this to know what the code for the safe will be 
     public enum Animal
     {
         Tiger,
@@ -27,20 +29,51 @@ public class RandomNumber : MonoBehaviour
 
     //In-game objects / TextObjects
 
+    public TextMeshPro tigerNumber;
+    public TextMeshPro wolfNumber;
+    public TextMeshPro foxNumber;
+    public TextMeshPro rabbitNumber;
 
-
-    void Start()
+    void Awake()
     {
-        numberOne = Randomize(minNumber,maxNumber);
+        //Decide the numbers
+        numberOne = Randomize(minNumber, maxNumber);
         numberTwo = Randomize(minNumber, maxNumber);
         numberThree = Randomize(minNumber, maxNumber);
         numberFour = Randomize(minNumber, maxNumber);
-        print(numberOne +""+ numberTwo +""+ numberThree +""+ numberFour);
+        print(numberOne + "" + numberTwo + "" + numberThree + "" + numberFour);
+        //Show the numbers
+        tigerNumber.text = numberOne.ToString();
+        wolfNumber.text = numberTwo.ToString();
+        foxNumber.text = numberThree.ToString();
+        rabbitNumber.text = numberFour.ToString();
+        switch (type)
+        {
+            case Animal.Tiger:
+                theNumber = numberOne;
+                break;
+            case Animal.Wolf:
+                theNumber = numberTwo;
+                break;
+            case Animal.Fox:
+                theNumber = numberThree;
+                break;
+            case Animal.Rabbit:
+                theNumber = numberFour;
+                break;
+            default:
+                break;
+        }
     }
     public int Randomize(int min, int max)
     {
         int number;
-        return number = Random.Range(min,max);
+        return number = Random.Range(min, max);
     }
+    public int GetNumber()
+    {
+        return theNumber;
+    }
+
 
 }
