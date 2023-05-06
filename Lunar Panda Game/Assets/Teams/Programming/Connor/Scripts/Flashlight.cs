e180d15a26515662faa9e9152ded075758ccb212
Eoin Matthews <5matte69@solent.ac.uk>
2023-05-07 00:55:44 +0100

    Fixed Torch

--- Lunar Panda Game/Assets/Teams/Programming/Connor/Scripts/Flashlight.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Connor/Scripts/Flashlight.cs
@@ -3,75 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//Script by Connor - if designers need any assistance feel free to dm or @ me on Discord
-
 public class Flashlight : MonoBehaviour
 {
-    private Light lightSource; //creating lightsource to assign at runtime
-
-    //const float maxBatteryLife = 60; //total battery life 
-    //float batteryLife = maxBatteryLife; //current battery life will change this later after designers say what they want
-
-    internal bool onOff = false; //Flashlight starts disabled this manages battery consumption 
-
-
-    //Matej changes - just flickering :)
-    //public float minWaitTime = 5;
-    //public float maxWaitTime = 8;
-
-    //public float stopMinWaitTime = 0;
-    //public float stopMaxWaitTime = 0.2f;
-
-    //public int minFlicks = 2;
-    //public int maxFlicks = 3;
+    private Light lightSource;
+    internal bool onOff = false;
 
     public Image UIon;
     public Image UIoff;
 
-    private bool isOn;
-    public string clipName;//Matej changes
+    public string clipName;
 
     public float intensity = 0f;
 
     public float minIntensity;
     public float maxIntensity;
 
+    public float wallIntensityChangeSpeed = 2.0f;
 
     void Start()
     {
-        lightSource = this.gameObject.GetComponent<Light>(); //assigning the spotlight to access it
-        isOn = true;
-
+        lightSource = this.gameObject.GetComponent<Light>();
         lightSource.type = LightType.Spot;
-        //StartCoroutine(Flashing());
-
-
     }
 
     void Update()
     {
-        toggleLight(); //runs the toggleLight function.
-
-        lightSource.intensity = intensity;
+        toggleLight();
 
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
         if (Physics.Raycast(transform.position, fwd, 2))
         {
-            Mathf.MoveTowards(intensity, minIntensity, Time.deltaTime / maxIntensity) ;
-            //intensity += Time.deltaTime * 60;
-
+            intensity = Mathf.Lerp(intensity, minIntensity, Time.deltaTime * wallIntensityChangeSpeed);
         }
-
         else
         {
-            Mathf.MoveTowards(intensity, maxIntensity, Time.deltaTime / minIntensity);
-            //intensity -= Time.deltaTime * 60;
-
+            intensity = Mathf.Lerp(intensity, maxIntensity, Time.deltaTime * wallIntensityChangeSpeed);
         }
 
-        //intensity = Mathf.Clamp(intensity, minIntensity, maxIntensity);
-
+        lightSource.intensity = intensity;
     }
 
     void toggleLight()
@@ -82,43 +52,18 @@ public class Flashlight : MonoBehaviour
 
             if (onOff)
             {
-                SoundEffectManager.GlobalSFXManager.PlaySFX(clipName);//Matej changes
+                SoundEffectManager.GlobalSFXManager.PlaySFX(clipName);
                 lightSource.enabled = true;
                 UIon.enabled = true;
                 UIoff.enabled = false;
             }
             else
             {
-                SoundEffectManager.GlobalSFXManager.PlaySFX(clipName);//Matej changes
+                SoundEffectManager.GlobalSFXManager.PlaySFX(clipName);
                 lightSource.enabled = false;
                 UIon.enabled = false;
                 UIoff.enabled = true;
             }
         }
     }
-
-    //IEnumerator Flashing()
-    //{
-    //    while (true)
-    //    {
-    //        if(onOff)
-    //        {
-    //            if (isOn)
-    //            {
-    //                lightSource.intensity = 28000;
-    //                yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
-    //                isOn = false;
-    //            }
-    //            if (!isOn)
-    //            {
-    //                for (int i = 0; i <= Random.Range(minFlicks, maxFlicks) * 2; i++)
-    //                {
-    //                    yield return new WaitForSeconds(Random.Range(stopMinWaitTime, stopMaxWaitTime));
-    //                    lightSource.intensity = Random.Range(5000, 10000);
-    //                    isOn = true;
-    //                }
-    //            }
-    //        } 
-    //    }
-    //}
 }
