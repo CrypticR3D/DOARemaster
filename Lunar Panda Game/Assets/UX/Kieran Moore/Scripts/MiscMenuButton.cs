7909d069d63e110468b8210726361d5acf67dbde
CrypticR3D <eoin95m@gmail.com>
2023-05-14 19:47:10 +0100

    EM_Update 4
    -Moved UX folders to main Dir

---
+++ Lunar Panda Game/Assets/UX/Kieran Moore/Scripts/MiscMenuButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MiscMenuButton : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject CurrentMenu;
+    public GameObject MenuToShow;
+    [SerializeField] GameObject firstButtonSelected;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        CurrentMenu.SetActive(false);
+        MenuToShow.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        if (firstButtonSelected != null)
+        {
+            EventSystem.current.SetSelectedGameObject(firstButtonSelected);
+        }         
+    }
+}
